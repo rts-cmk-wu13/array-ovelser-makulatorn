ba7c85d6822a6592ad5d60c0f51effbd9dcdd46f
sasha <sasha.friis@icloud.com>
2025-02-07 11:31:40 +0100

    opgave løst, lidt sent, men lavet

--- arrays.js
+++ arrays.js
@@ -6,4 +6,54 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // --- Løs opgaverne herunder --- 
 
+    //opgave 1
+    let newNumbers = numbersArray.filter(function(number) {
+        return number > 20;
+    })
+    console.log(newNumbers);
+
+    // opgave 2
+    catsArray.push("Sniffles");
+    console.log("opgave 2", catsArray);
+
+    //opgave 3 
+    let hasTrixie = dogsArray.includes("Trixie")
+    console.log("Opgave 3", hasTrixie);
+
+    let hasBaxter = dogsArray.some(function(dog){ return dog == "Baxter"})
+    let hasBaxterArrow = dogsArray.some((dog) => dog == "Baxter")
+    console.log("opgave 3b", hasBaxter);
+
+    //opgave 4
+    let hasBagheera = catsArray.find((cat) => cat == "Bagheera")
+    let hasBagheeraFunction = catsArray.find(function(cat){return cat = "Bagheera"})
+    console.log("opgave 4a", hasBagheera);
+
+    let hasSalem = catsArray.find(cat => cat == "Salem")
+    console.log(("opgave 4b", hasSalem));
+
+    //opgave 5
+
+    let multiplied = numbersArray.map((number) => number * 3)
+    console.log("opgave 5", multiplied);
+
+    //opgave 6
+    let dogsString = dogsArray.join("")
+    console.log("opgave 5", dogsString);
+
+    //opgave 7 
+
+    let mangoIndex = fruitsArray.indexOf("Mango")
+    console.log("Opgave 7a", mangoIndex);
+
+    let blueberryIndex = fruitsArray.indexOf("Blåbær")
+    console.log("Opgave 7b", blueberryIndex);
+
+    //opgave 8 
+    let dogsModified = dogsArray.filter(dog => dog != "Polly")
+    console.log(dogsModified);
+
+    //ekstra opgave 
+    let sum = numbersArray.reduce((result, number) => result + number, 500)
+    console.log("ekstraopgave", sum);
 }) // ends DOMContentLoaded
